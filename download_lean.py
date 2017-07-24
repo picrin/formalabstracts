753a1c273954e2f8f2e26fa973d0268728694c3e
picrin <Adam Kurkiewicz>
2017-07-24 10:50:29 +0100

    bash together a fugly python script

--- download_lean.py
+++ download_lean.py
@@ -1,3 +1,5 @@
+import tarfile, subprocess, sys
+
 lean_URL = "https://github.com/leanprover/lean/releases/download/v3.2.0/lean-3.2.0-linux.tar.gz"
 
 import urllib.request
@@ -13,3 +15,13 @@ elif (fname.endswith("tar")):
     tar = tarfile.open(fname, "r:")
     tar.extractall()
     tar.close()
+
+filename = "lean-3.2.0-linux.tar.gz"
+
+tar = tarfile.open(filename, "r:gz")
+tar.extractall()
+tar.close()
+
+exit = subprocess.run("lean-3.2.0-linux/bin/lean --make meta_data.lean", shell=True)
+
+sys.exit(exit.returncode)
