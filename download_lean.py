5d6a74fae8a22b95e1b3905f054e113eb83d4bab
picrin <Adam Kurkiewicz>
2017-07-24 10:54:48 +0100

    fix python script

--- download_lean.py
+++ download_lean.py
@@ -5,17 +5,6 @@ lean_URL = "https://github.com/leanprover/lean/releases/download/v3.2.0/lean-3.2
 import urllib.request
 urllib.request.urlretrieve(lean_URL, lean_URL.split("/")[-1])
 
-
-import tarfile
-if (fname.endswith("tar.gz")):
-    tar = tarfile.open(fname, "r:gz")
-    tar.extractall()
-    tar.close()
-elif (fname.endswith("tar")):
-    tar = tarfile.open(fname, "r:")
-    tar.extractall()
-    tar.close()
-
 filename = "lean-3.2.0-linux.tar.gz"
 
 tar = tarfile.open(filename, "r:gz")
